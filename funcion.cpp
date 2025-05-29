4acac6e3d1e387de15b5e9f3369e977d7403dbd3
JosueMaquera <jmaquerap@unjbg.edu.pe>
2025-05-29 00:09:37 -0500

    Segundo commit: serie de fibonacci con la funciones imprimir y sumar

--- funcion.cpp
+++ funcion.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
 using namespace std;
+int imprimir_sumar(int n);
 int main() {
-    int n,a,b,i,serie;
+    int n, suma;
     cout<<"Ingrese la cantidad de numeros de Fibonacci a imprimir: ";
-    cin >> n;
-    a=0;
-	b=1;
-    for (i=0;i<n;i++) {
-        cout<<a<<endl;           
-        serie=a+b;           
-        a=b;                       
+    cin>>n;
+    suma=imprimir_sumar(n);
+    cout<<"La suma de la serie es: "<<suma<<endl;
+    return 0;
+}
+int imprimir_sumar(int n) {
+    int a,b,suma,serie;
+	a=0;
+	b=1; 
+	suma=0;
+    for (int i = 0; i < n; i++) {
+        cout << a << endl;
+        suma=suma+a;
+        serie=a+b;
+        a=b;
         b=serie;
     }
-    return 0;
+    return suma;
 }
