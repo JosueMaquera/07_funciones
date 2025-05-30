b15c8c5a2d47907f25058f102e66056795e3b120
JosueMaquera <jmaquerap@unjbg.edu.pe>
2025-05-30 17:34:09 -0500

    Añadir función menor y mayor, corregir la lógica de validación de datos

--- funcion2.cpp
+++ funcion2.cpp
@@ -1,28 +1,51 @@
 #include <iostream>
 using namespace std;
+int determinar_menor(int num1 , int num2);
+int determinar_mayor(int num1 , int num2);
 bool validacion_datos(int num1, int num2);
 int main() {    
-    int num1,num2,i,dividendo,cociente;
+    int num1,num2,i,dividendo,cociente,divisor;
     cin>>num1>>num2;
     cociente=0;
-    dividendo=num1;
-    if (validacion_datos(num1,num2)) {
+    if (!validacion_datos(num1,num2)) {
         cout<<"Datos invalidos: ambos deben ser positivos"<<endl;
         return 1;
     }
-    while (dividendo>num2) {
-        dividendo = dividendo - num2;
+    dividendo=determinar_mayor(num1,num2);
+    divisor=determinar_menor(num1,num2);
+    while (dividendo>=divisor) {
+        dividendo = dividendo - divisor;
         cociente = cociente + 1;
     }
     cout<<cociente<<endl;
     cout<<dividendo<<endl;
     return 0;
 }
-bool validacion_datos(int num1 , int num2) {
-    if (num1>0 && num2>0) {
-        return false;
+int determinar_mayor(int num1 , int num2) {
+    int mayor;
+    if (num1>num2) {
+        mayor=num1;
     }
     else {
+        mayor=num2;
+    }
+    return mayor;
+}
+int determinar_menor(int num1 , int num2) {
+    int menor;
+    if (num1>num2) {
+        menor=num2;
+    }
+    else {
+        menor=num1;
+    }
+    return menor;
+}
+bool validacion_datos(int num1 , int num2) {
+    if (num1>0 && num2>0) {
         return true;
+    }
+    else {
+        return false;
     }
 }
