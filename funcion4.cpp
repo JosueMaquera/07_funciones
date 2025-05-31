4be7b6b2401f79c593e57c5bb3fe4e85e50c5760
JosueMaquera <jmaquerap@unjbg.edu.pe>
2025-05-31 15:16:11 -0500

    Agregar funciones determinar_perfecto, validar_datos e imprimir_resultados

--- funcion4.cpp
+++ funcion4.cpp
@@ -1,27 +1,43 @@
 #include <iostream>
 using namespace std;
+int determinar_perfecto(int n);
+bool validar_datos (int n);
+void imprimir_resultados(int n, int suma);
 int main() {
-    int n, i, suma;
+    int n,suma;
     cout << "Ingresa un numero entero positivo: ";
     cin >> n;
-    suma = 0;
+    if (!validar_datos(n)) {
+        return 1;
+    }
+    suma = determinar_perfecto(n);
+    imprimir_resultados(n,suma);    
+    return 0;
+}
+bool validar_datos(int n) {
     if (n <= 0) {
         cout << "----------------------------------"<<endl;
         cout << n << " no es un numero perfecto porque debe ser positivo";
-        return 0;
-    }
+        return false;
+    } 
+    return true; 
+}
+int determinar_perfecto(int n) {
+    int suma,i;
+    suma=0;
     for (i = 1; i < n; i++) {
         if (n % i == 0) {
             suma = suma + i;
         }
     }
+    return suma;
+}
+void imprimir_resultados(int n, int suma) {
+    cout << "----------------------------------"<<endl; 
     if (n == suma) {
-        cout << "----------------------------------"<<endl;
         cout << n << " es un numero perfecto";
     }
     else {
-        cout << "----------------------------------"<<endl;
         cout << n << " no es un numero perfecto";
     }
-    return 0;
 }
