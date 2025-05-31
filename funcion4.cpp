95aa025b4d2fb555f127d929bde03597fc1ab849
JosueMaquera <jmaquerap@unjbg.edu.pe>
2025-05-31 15:34:30 -0500

    Añadir función determinar_primo para verificar números primos

--- funcion4.cpp
+++ funcion4.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 using namespace std;
 int determinar_perfecto(int n);
-bool validar_datos (int n);
-void imprimir_resultados(int n, int suma);
+int determinar_primo(int n);
+bool validar_datos(int n);
+void imprimir_resultados(int n, int suma, int divisores);
 int main() {
-    int n,suma;
+    int n, suma, divisores;
     cout << "Ingresa un numero entero positivo: ";
     cin >> n;
     if (!validar_datos(n)) {
         return 1;
     }
     suma = determinar_perfecto(n);
-    imprimir_resultados(n,suma);    
+    divisores = determinar_primo(n); 
+    imprimir_resultados(n,suma,divisores);    
     return 0;
 }
 bool validar_datos(int n) {
@@ -32,12 +34,25 @@ int determinar_perfecto(int n) {
     }
     return suma;
 }
-void imprimir_resultados(int n, int suma) {
+void imprimir_resultados(int n, int suma, int divisores) {
     cout << "----------------------------------"<<endl; 
+    if (divisores == 2) {
+        cout << n << " es un numero primo"<<endl;
+    }
     if (n == suma) {
         cout << n << " es un numero perfecto";
     }
     else {
         cout << n << " no es un numero perfecto";
     }
+}
+int determinar_primo(int n) {
+    int divisores,i;
+    divisores = 0;
+    for (i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            divisores = divisores + 1;
+        }
+    }
+    return divisores; 
 }
