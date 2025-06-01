d36c78cf6fa3529a72c0ffbce09a4319196d7c11
JosueMaquera <jmaquerap@unjbg.edu.pe>
2025-06-01 16:08:40 -0500

    Implementar función que calcula los divisores propios

--- funcion5.cpp
+++ funcion5.cpp
@@ -1,32 +1,25 @@
 #include <iostream> 
 using namespace std;
+int suma_divisores_propios(int num);
 int main() {
-    int num1,num2,divisores1,divisores2,i,e,suma1,suma2;
+    int num1,num2,suma1,suma2;
     cin >> num1 >> num2;
-    divisores1 = 0;
-    divisores2 = 0;
-    suma1 = 0;
-    suma2 = 0;
-    for (i = 1; i <= num1; i++) {
-        if (num1 % i == 0) {
-            divisores1 = divisores1 + 1; 
-            suma1 = suma1 + i;
-            //cout << i << endl;
-        }
-    }
-    for (e = 1; e <= num2; e++) {
-        if (num2 % e == 0) {
-            divisores2 = divisores2 + 1;
-            suma2 = suma2 + e;
-            //cout << e <<endl;
-        }
-    }
-    //cout << suma1 << endl;
-    //cout << suma2 << endl;
-    if (suma1 == suma2) {
+    suma1 = suma_divisores_propios(num1);
+    suma2 = suma_divisores_propios(num2);
+    if (num1 == suma2 && num2 == suma1) {
         cout << "Son numeros amigos" <<endl;
         return 1;
     }
     cout << "No son numeros amigos" <<endl;
     return 0;
+}
+int suma_divisores_propios(int num) {
+    int suma,i;
+    suma = 0;
+    for (i = 1; i < num; i++) {
+        if (num % i == 0) {
+            suma = suma + i;
+        }
+    }
+    return suma;
 }
