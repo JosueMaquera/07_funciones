7b428a728d302c61c96d6ecad2cf42b66135af18
JosueMaquera <jmaquerap@unjbg.edu.pe>
2025-06-02 15:21:35 -0500

    Añadir función determinar_primo

--- funcion6.cpp
+++ funcion6.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
 using namespace std;
+int determinar_primo(int num1);
 int main() {
     int num1,b,c,d,divisores;
     cin >> num1;
+    divisores = determinar_primo(num1);
+    if (divisores == 2) {
+        cout << "Es primo";
+        return 1;
+    }
+    cout << "No es primo";
+}
+int determinar_primo(int num1) {
+    int divisores; 
     divisores = 0;
     for (int i = 1; i <= num1; i++) {
         if (num1 % i == 0) {
             divisores = divisores + 1;
         }
     }
-    if (divisores == 2) {
-        cout << "Es primo";
-        return 1;
-    }
-    cout << "No es primo";
+    return divisores;
 }
