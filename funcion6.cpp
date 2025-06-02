d36d85619263688c8dbc8d695255949387b460bf
JosueMaquera <jmaquerap@unjbg.edu.pe>
2025-06-02 15:42:45 -0500

    Eliminar variables no utilizadas

--- funcion6.cpp
+++ funcion6.cpp
@@ -2,30 +2,30 @@
 using namespace std;
 bool validar_datos(int num1);
 int determinar_primo(int num1);
-void imprimir_resultados(int divisores);
+void imprimir_resultados(int cantidad_divisores);
 int main() {
-    int num1,b,c,d,divisores;
+    int num1,cantidad_divisores;
     cin >> num1;
     if (validar_datos(num1)) {
         cout << "Ingrese un numero positivo";
         return 1;
     }
-    divisores = determinar_primo(num1);
-    imprimir_resultados(divisores);
+    cantidad_divisores = determinar_primo(num1);
+    imprimir_resultados(cantidad_divisores);
     return 0;
 }
 int determinar_primo(int num1) {
-    int divisores; 
-    divisores = 0;
+    int cantidad_divisores; 
+    cantidad_divisores = 0;
     for (int i = 1; i <= num1; i++) {
         if (num1 % i == 0) {
-            divisores = divisores + 1;
+            cantidad_divisores = cantidad_divisores + 1;
         }
     }
-    return divisores;
+    return cantidad_divisores;
 }
-void imprimir_resultados(int divisores) {
-    if (divisores == 2) {
+void imprimir_resultados(int cantidad_divisores) {
+    if (cantidad_divisores == 2) {
         cout << "Es primo";
     } else {
         cout << "No es primo";
