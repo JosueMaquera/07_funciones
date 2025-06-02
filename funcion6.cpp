e8b5afaff64b8622abb4cabe910a300823463342
JosueMaquera <jmaquerap@unjbg.edu.pe>
2025-06-02 15:34:12 -0500

    Añadir función validar_datos

--- funcion6.cpp
+++ funcion6.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 using namespace std;
+bool validar_datos(int num1);
 int determinar_primo(int num1);
 void imprimir_resultados(int divisores);
 int main() {
     int num1,b,c,d,divisores;
     cin >> num1;
+    if (validar_datos(num1)) {
+        cout << "Ingrese un numero positivo";
+        return 1;
+    }
     divisores = determinar_primo(num1);
     imprimir_resultados(divisores);
+    return 0;
 }
 int determinar_primo(int num1) {
     int divisores; 
@@ -24,4 +30,7 @@ void imprimir_resultados(int divisores) {
     } else {
         cout << "No es primo";
     }
+}
+bool validar_datos(int num1) {
+    return num1 <= 0;
 }
