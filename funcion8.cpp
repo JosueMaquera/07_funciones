93a789fcba737a11e4bebfb72291dff32b20ce02
JosueMaquera <jmaquerap@unjbg.edu.pe>
2025-06-03 21:12:57 -0500

    Añadir función imprimir_resultados

--- funcion8.cpp
+++ funcion8.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int minimo_comun_multiplo(int, int, int);
 int maximo_comun_divisor(int, int);
 bool validar_datos(int, int);
+void imprimir_resultados(int, int);
 int main() {
     int mcm, mcd, num1 , num2;
     cin >> num1 >> num2;
@@ -12,8 +13,7 @@ int main() {
     }
     mcd = maximo_comun_divisor(num1,num2);
     mcm = minimo_comun_multiplo(num1,num2,mcd);
-    cout << "El maximo comun divisor es: " << mcd << endl;
-    cout << "El minimo comun multiplo es: " << mcm << endl;
+    imprimir_resultados(mcd,mcm);
 
     return 0;
 }
@@ -35,4 +35,8 @@ int minimo_comun_multiplo(int num1, int num2, int mcd) {
 }
 bool validar_datos(int num1, int num2) {
     return num1 <= 0 || num2 <= 0;
+}
+void imprimir_resultados(int mcd, int mcm) {
+    cout << "El maximo comun divisor es: " << mcd << endl;
+    cout << "El minimo comun multiplo es: " << mcm << endl;
 }
