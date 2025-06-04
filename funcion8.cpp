1e529fceb85e5b858c8cb1877ff83226733b760a
JosueMaquera <jmaquerap@unjbg.edu.pe>
2025-06-03 20:58:06 -0500

    Añadir funcion maximo_comun_divisor

--- funcion8.cpp
+++ funcion8.cpp
@@ -1,8 +1,19 @@
 #include<iostream>
 using namespace std;
+int minimo_comun_multiplo(int, int, int);
+int maximo_comun_divisor(int, int);
 int main() {
     int mcm, mcd, num1 , num2, i;
     cin >> num1 >> num2;
+    mcd = maximo_comun_divisor(num1,num2);
+    mcm = (num1 * num2) / mcd;
+    cout << "El maximo comun divisor es: " << mcd << endl;
+    cout << "El minimo comun multiplo es: " << mcm << endl;
+
+    return 0;
+}
+int maximo_comun_divisor(int num1, int num2) {
+    int i, mcd;
     i = 1;
     while (num1 >= i && num2 >= i) {
         if (num1 % i == 0 && num2 % i == 0) {
@@ -10,9 +21,5 @@ int main() {
         }
         i = i + 1;
     }
-    mcm = (num1 * num2) / mcd;
-    cout << "El maximo comun divisor es: " << mcd << endl;
-    cout << "El minimo comun multiplo es: " << mcm << endl;
-
-    return 0;
+    return mcd;
 }
